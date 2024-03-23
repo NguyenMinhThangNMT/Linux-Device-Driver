b5e8f4d7ee617ab8fbcd84290af808ce98798e9e
NguyenMinhThangNMT <125476408+NguyenMinhThangNMT@users.noreply.github.com>
2024-03-23 20:18:17 +0700

    Update My_Driver.c

--- Device_Driver/My_Driver.c
+++ Device_Driver/My_Driver.c
@@ -1,97 +1,142 @@
-#include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/proc_fs.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
+#include <linux/init.h>
+#include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/device.h>
+#include <linux/uaccess.h>
 
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Johannes 4 GNU/Linux");
+MODULE_DESCRIPTION("Registers a device nr. and implement some callback functions");
 
-//Dinh nghia Deviece name
-#define DRIVER_NAME "my_driver"
-#define DRIVER_CLASS "MymoduleClass"
+/* Buffer for data */
+static char buffer[255];
+static int buffer_pointer = 0;
 
+/* Variables for device and device class */
 static dev_t my_device_nr;
 static struct class *my_class;
 static struct cdev my_device;
 
-// Ham open file
+#define DRIVER_NAME "dummydriver"
+#define DRIVER_CLASS "MyModuleClass"
+
+/**
+ * @brief Read data out of the buffer
+ */
+static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	/* Get amount of data to copy */
+	to_copy = min(count, buffer_pointer);
+
+	/* Copy data to user */
+	not_copied = copy_to_user(user_buffer, buffer, to_copy);
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief Write data to buffer
+ */
+static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+
+	/* Get amount of data to copy */
+	to_copy = min(count, sizeof(buffer));
+
+	/* Copy data to user */
+	not_copied = copy_from_user(buffer, user_buffer, to_copy);
+	buffer_pointer = to_copy;
+
+	/* Calculate data */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief This function is called, when the device file is opened
+ */
 static int driver_open(struct inode *device_file, struct file *instance) {
-    printk("device open was called\n");
-    return 0;
+	printk("dev_nr - open was called!\n");
+	return 0;
 }
 
-// Ham close file
+/**
+ * @brief This function is called, when the device file is opened
+ */
 static int driver_close(struct inode *device_file, struct file *instance) {
-    printk("device close was called\n");
-    return 0;
+	printk("dev_nr - close was called!\n");
+	return 0;
 }
 
-ssize_t driver_read(struct file *file, char __user *user, size_t size, loff_t *off)
-{
-    return copy_to_user(user, "Hello!\n", 7) ? 0 : 7;
-}
-
-ssize_t driver_write(struct file *file, const char __user *user_buffer, size_t size, loff_t *off)
-{
-    char user_data[10];
-    memset(user_data, 0, 10);
-    if (copy_from_user(user_data, user_buffer, size)) 
-        return 0;
- 
-    printk("user write : %s", user_data);
-    return size;
-}
-
-static const struct file_operations my_fops = {
-    .owner = THIS_MODULE,
-    .open = driver_open,
-    .release = driver_close,
-    .read = driver_read,
-    .write = driver_write,
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_open,
+	.release = driver_close,
+	.read = driver_read,
+	.write = driver_write
 };
-static int my_driver_init(void)
-{
-    printk("Hello Device Driver\n");
-    //Dang ki 
-   if(alloc_chrdev_region(&my_device_nr,0,1,DRIVER_NAME)<0){
-    printk("Device could not be allocated\n");
-    return -1;
-   }
-   printk("my_driver : Major:%d,Minor :%d \n",my_device_nr>>20,my_device_nr&0xfffff);
-   if((my_class = class_create (THIS_MODULE,DRIVER_CLASS))==NULL){
-    printk("Device class can not be created");
-    unregister_chrdev_region(my_device_nr,1);
-    return -1;
-   }
-   if((device_create(my_class,NULL,my_device_nr,NULL,DRIVER_NAME))==NULL){
-    printk("can not create device file");
-    class_destroy(my_class);
-    return -1;
-   }
-   cdev_init(&my_device, &my_fops);
 
-   if(cdev_add(&my_device,my_device_nr,1)==1){
-    printk("can not create device file");
-    device_destroy(my_class,my_device_nr);
-    return -1;
-   }
-    return 0;
+/**
+ * @brief This function is called, when the module is loaded into the kernel
+ */
+static int __init ModuleInit(void) {
+	int retval;
+	printk("Hello, Kernel!\n");
+
+	/* Allocate a device nr */
+	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
+		printk("Device Nr. could not be allocated!\n");
+		return -1;
+	}
+	printk("read_write - Device Nr. Major: %d, Minor: %d was registered!\n", my_device_nr >> 20, my_device_nr & 0xfffff);
+
+	/* Create device class */
+	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("Device class can not be created!\n");
+		goto ClassError;
+	}
+
+	/* create device file */
+	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL) {
+		printk("Can not create device file!\n");
+		goto FileError;
+	}
+
+	/* Initialize device file */
+	cdev_init(&my_device, &fops);
+
+	/* Regisering device to kernel */
+	if(cdev_add(&my_device, my_device_nr, 1) == -1) {
+		printk("Registering of device to kernel failed!\n");
+		goto AddError;
+	}
+
+	return 0;
+AddError:
+	device_destroy(my_class, my_device_nr);
+FileError:
+	class_destroy(my_class);
+ClassError:
+	unregister_chrdev_region(my_device_nr, 1);
+	return -1;
 }
 
-static void my_driver_exit(void)
-{
-    printk("Goodbye Device Driver!\n");
-    cdev_del(&my_device);
-    device_destroy(my_class,my_device_nr);
-    class_destroy(my_class);
-    unregister_chrdev_region(my_device_nr,1);
-    return;
+/**
+ * @brief This function is called, when the module is removed from the kernel
+ */
+static void __exit ModuleExit(void) {
+	cdev_del(&my_device);
+	device_destroy(my_class, my_device_nr);
+	class_destroy(my_class);
+	unregister_chrdev_region(my_device_nr, 1);
+	printk("Goodbye, Kernel\n");
 }
 
-module_init(my_driver_init);
-module_exit(my_driver_exit);
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("NMT");
-MODULE_VERSION("1.0");
+module_init(ModuleInit);
+module_exit(ModuleExit);
